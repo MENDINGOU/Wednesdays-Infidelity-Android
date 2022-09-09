89c472e58f1de44d8097aaa72bae1e5fbdf9f927
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-09-09 17:04:15 -0500

    Add files via upload

--- source/states/options/GraphicsSettingsSubState.hx
+++ source/states/options/GraphicsSettingsSubState.hx
@@ -1,34 +1,31 @@
-package states.options;
+package options;
 
 #if desktop
-import util.Discord.DiscordClient;
+import Discord.DiscordClient;
 #end
-import data.ClientPrefs;
-import flash.text.TextField;
 import flash.text.TextField;
 import flixel.FlxCamera;
 import flixel.FlxG;
-import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.FlxSprite;
-import flixel.FlxSubState;
 import flixel.addons.display.FlxGridOverlay;
-import flixel.graphics.FlxGraphic;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.input.keyboard.FlxKey;
 import flixel.math.FlxMath;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
-import flixel.util.FlxColor;
-import flixel.util.FlxSave;
 import flixel.util.FlxTimer;
-import gameObjects.*;
-import haxe.Json;
-import input.Controls;
-import lime.utils.Assets;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
 import openfl.Lib;
-import states.options.BaseOptionsMenu;
 
 using StringTools;
 
@@ -37,22 +34,38 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 	public function new()
 	{
 		title = 'Graphics';
-		rpcTitle = 'Graphics Settings Menu'; // for Discord Rich Presence
+		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
 
-		var option:Option = new Option('Anti-Aliasing', 'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
-			'globalAntialiasing', 'bool', true);
+		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
+		var option:Option = new Option('Low Quality', //Name
+			'If checked, disables some background details,\ndecreases loading times and improves performance.', //Description
+			'lowQuality', //Save data variable name
+			'bool', //Variable type
+			false); //Default value
+		addOption(option);
+
+		var option:Option = new Option('Anti-Aliasing',
+			'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
+			'globalAntialiasing',
+			'bool',
+			true);
 		option.showBoyfriend = true;
-		option.onChange = onChangeAntiAliasing; // Changing onChange is only needed if you want to make a special interaction after it changes the value
+		option.onChange = onChangeAntiAliasing; //Changing onChange is only needed if you want to make a special interaction after it changes the value
 		addOption(option);
 
-		var option:Option = new Option('Shaders', "Uncheck this if you don't want any Shaders!", 'shaders', 'bool', true);
-		addOption(option);
+                var option:Option = new Option('Shaders', //Name
+	            'If uncheked, disables shaders and effects\n(Recommended to disable if you have a low pc or phone) ', //Description
+	            'shaders', //Save data variable name
+	            'bool', //Variable type
+	            true); //Default value
+                addOption(option);
 
-		var option:Option = new Option('Intensive Shaders', "Uncheck this if you don't want to run Intensive Shaders!", 'intensiveShaders', 'bool', true);
-		addOption(option);
-
-		#if !html5 // Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
-		var option:Option = new Option('Framerate', "Pretty self explanatory, isn't it?", 'framerate', 'int', 60);
+		#if !html5 //Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
+		var option:Option = new Option('Framerate',
+			"Pretty self explanatory, isn't it?",
+			'framerate',
+			'int',
+			60);
 		addOption(option);
 
 		option.minValue = 60;
@@ -62,14 +75,14 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		#end
 
 		/*
-			var option:Option = new Option('Persistent Cached Data',
-				'If checked, images loaded will stay in memory\nuntil the game is closed, this increases memory usage,\nbut basically makes reloading times instant.',
-				'imagesPersist',
-				'bool',
-				false);
-			option.onChange = onChangePersistentData; //Persistent Cached Data changes FlxGraphic.defaultPersist
-			addOption(option);
-		 */
+		var option:Option = new Option('Persistent Cached Data',
+			'If checked, images loaded will stay in memory\nuntil the game is closed, this increases memory usage,\nbut basically makes reloading times instant.',
+			'imagesPersist',
+			'bool',
+			false);
+		option.onChange = onChangePersistentData; //Persistent Cached Data changes FlxGraphic.defaultPersist
+		addOption(option);
+		*/
 
 		super();
 	}
@@ -78,10 +91,9 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 	{
 		for (sprite in members)
 		{
-			var sprite:Dynamic = sprite; // Make it check for FlxSprite instead of FlxBasic
-			var sprite:FlxSprite = sprite; // Don't judge me ok
-			if (sprite != null && (sprite is FlxSprite) && !(sprite is FlxText))
-			{
+			var sprite:Dynamic = sprite; //Make it check for FlxSprite instead of FlxBasic
+			var sprite:FlxSprite = sprite; //Don't judge me ok
+			if(sprite != null && (sprite is FlxSprite) && !(sprite is FlxText)) {
 				sprite.antialiasing = ClientPrefs.globalAntialiasing;
 			}
 		}
@@ -89,7 +101,7 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 
 	function onChangeFramerate()
 	{
-		if (ClientPrefs.framerate > FlxG.drawFramerate)
+		if(ClientPrefs.framerate > FlxG.drawFramerate)
 		{
 			FlxG.updateFramerate = ClientPrefs.framerate;
 			FlxG.drawFramerate = ClientPrefs.framerate;
