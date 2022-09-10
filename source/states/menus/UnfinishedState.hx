e762b6e71b4b2f2404fb246fb95f4df30be74ee8
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-09-09 21:44:07 -0500

    Update UnfinishedState.hx

--- source/states/menus/UnfinishedState.hx
+++ source/states/menus/UnfinishedState.hx
@@ -52,6 +52,9 @@ class UnfinishedState extends MusicBeatState
 
 	override function create()
 	{
+                #if android 
+                addVirtualPad(NONE, A);
+                #end
 		super.create();
 
 		FlxG.camera.alpha = 0;
@@ -89,7 +92,7 @@ class UnfinishedState extends MusicBeatState
 	{
 		super.update(elapsed);
 
-		if (canPress && (FlxG.keys.justPressed.SPACE || FlxG.keys.justPressed.ENTER))
+		if (canPress && (FlxG.keys.justPressed.SPACE || _virtualPad.buttonA.justPressed)
 		{
 			canPress = false;
 
